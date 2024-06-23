69879d510ea7f3c73fa15841728b7eeeabad4c0b
jeevajith123 <e19227@eng.pdn.ac.lk>
2024-06-23 12:52:44 +0530

    Update Notfound.jsx

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# -salary-calculator-2024-Q1--jeevajith123
+# salary-calculator-2024-Q1--jeevajith123
+
+## Description
+- This React application is built using [create-react-app].
+- It uses [Tailwind CSS]
+- The application is generated in Javascript.
--- salary-calculator/src/Routes.jsx
+++ salary-calculator/src/Routes.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom'; // Import Routes
 import SalaryCalculator from "./pages/SalaryCalculator";
+import NotFound from "./pages/NotFound";
 
 const ProjectRoutes = () => {
     return (
         <Routes> {/* Use Routes to wrap Route components */}
             <Route path="/" element={<SalaryCalculator />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     );
 };
