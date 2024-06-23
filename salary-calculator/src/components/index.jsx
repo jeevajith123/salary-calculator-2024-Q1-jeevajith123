257e73a092e6996a2a3178015968398cc297eb5b
jeevajith123 <e19227@eng.pdn.ac.lk>
2024-06-23 19:14:19 +0530

    Update index.jsx

--- salary-calculator/src/components/index.jsx
+++ salary-calculator/src/components/index.jsx
@@ -1,7 +1,6 @@
 import {Text} from './Text';
-import {Button} from './Button';
 import {Input} from './Input';
 import {Heading} from './Heading';
 import {CheckBox} from './CheckBox';
 
-export {Text, Button, Input, Heading, CheckBox};
+export {Text, Input, Heading, CheckBox};
